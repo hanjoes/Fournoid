2daf91a47c666ad4b54b29e2f0c8623afcb9da20
daniel0128 <survey600@qq.com>
2016-05-01 13:54:02 -0700

    AIController Files
    empty files

---
+++ Source/Fournoid/FournoidAIController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AIController.h"
+#include "FournoidAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FOURNOID_API AFournoidAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

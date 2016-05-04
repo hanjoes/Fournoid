09971df53c140ce10d95681753e262079c8f76ca
Hanzhou Shi <hanzhou87@gmail.com>
2016-05-04 16:16:52 -0700

    Clean things up.

--- Source/Fournoid/FournoidGameMode.cpp
+++ Source/Fournoid/FournoidGameMode.cpp
@@ -9,7 +9,7 @@ AFournoidGameMode::AFournoidGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FournoidAssets/Blueprints/PlayerCharacter_BP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class

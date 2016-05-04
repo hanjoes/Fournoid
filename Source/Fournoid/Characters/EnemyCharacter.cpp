7af40c4b5c5399ff5a62db67b039d382f2fc09a0
Hanzhou Shi <hanzhou87@gmail.com>
2016-05-04 15:56:21 -0700

    Added reconstructed project.

---
+++ Source/Fournoid/Characters/EnemyCharacter.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Fournoid.h"
+#include "EnemyCharacter.h"
+
+
+// Sets default values
+AEnemyCharacter::AEnemyCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	SetupCollisionBehavior();
+}
+
+// Called when the game starts or when spawned
+void AEnemyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AEnemyCharacter::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+	
+}
+
+void AEnemyCharacter::SetupCollisionBehavior()
+{
+}
+
+void AEnemyCharacter::ReceiveDamage(float Damage)
+{
+	Super::ReceiveDamage(Damage);
+	
+	if (Health <= 0)
+	{
+		GetMesh()->SetSimulatePhysics(true);
+	}
+}
+

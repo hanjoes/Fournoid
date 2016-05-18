edce4348def6ee06efb12088306fbd0370d8fb5b
Hanzhou Shi <hanzhou87@gmail.com>
2016-05-17 22:15:24 -0700

    creating/joining session

--- Source/Fournoid/Fournoid.Build.cs
+++ Source/Fournoid/Fournoid.Build.cs
@@ -6,7 +6,15 @@ public class Fournoid : ModuleRules
 {
 	public Fournoid(TargetInfo Target)
 	{
-PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "OnlineSubsystem", "OnlineSubsystemUtils" });
         PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+        {
+            DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
+        }
     }
+
 }

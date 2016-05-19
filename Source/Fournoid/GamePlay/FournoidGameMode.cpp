d90dab326850790173efe7844ab6361fd7b49243
aisle2678 <yhuang103@gmail.com>
2016-05-18 21:47:00 -0700

    minimap cam following

--- Source/Fournoid/GamePlay/FournoidGameMode.cpp
+++ Source/Fournoid/GamePlay/FournoidGameMode.cpp
@@ -14,9 +14,15 @@ AFournoidGameMode::AFournoidGameMode()
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawn(TEXT("/Game/FournoidAssets/Blueprints/PlayerCharacter_BP"));
 	DefaultPawnClass = PlayerPawn.Class;
-	PlayerControllerClass = AFournoidPlayerController::StaticClass();
 	PlayerStateClass = AFournoidPlayerState::StaticClass();
 	GameStateClass = AFournoidGameState::StaticClass();
+
+	// Player Controller class
+	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerOb(TEXT("/Game/FournoidAssets/Blueprints/FournoidPlyaerController_BP"));
+	if (PlayerControllerOb.Class != NULL)
+	{
+		PlayerControllerClass = PlayerControllerOb.Class;
+	}
 
 	// HUD class
 	static ConstructorHelpers::FClassFinder<AHUD> TheHUDOb(TEXT("/Game/UserInterface/MyFournoidHUD.MyFournoidHUD_C"));
